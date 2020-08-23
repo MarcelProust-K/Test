e64038156b0eca9ab6587d9cd293ac0837deadad
unknown <kongxingshou@outlook.com>
2020-08-23 19:36:12 +0800

    add test_master fun

--- thread_01.py
+++ thread_01.py
@@ -24,6 +24,11 @@ def test04(times):
     time.sleep(3)
 
 
+def test_master(times):
+    print("this is test fun:")
+    time.sleep(3)
+
+
 def main():
     t1 = threading.Thread(target=test01, args=(3, ))
     t2 = threading.Thread(target=test02, args=(3, ))
