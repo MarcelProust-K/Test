a511f56c6e382448f4dd7466dcf15d1bb6bd06a1
unknown <kongxingshou@outlook.com>
2020-08-23 19:33:45 +0800

    add test_dev  fun

--- thread_01.py
+++ thread_01.py
@@ -28,6 +28,10 @@ def test_master(times):
     print("this is test fun:")
     time.sleep(3)
 
+def test_dev(times):
+    print("this is test fun:")
+    time.sleep(3)
+
 
 def main():
     t1 = threading.Thread(target=test01, args=(3, ))
